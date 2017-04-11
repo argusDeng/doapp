6985149eb235e3f427ca0d85225f4d999a7328d5
dengerhuan <argus.deng@nokia.com>
2017-04-11 20:00:33 +0800

    modify dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  numberOfGoblins: number;
+  progress = 1;
+  lastDialogResult: string;
+  folders: any= [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
+
+
+  constructor(private _dialog: MdDialog, private _snackbar: MdSnackBar) {
+    // Update the value for the progress-bar on an interval.
+    setInterval(() => {
+      this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100;
+    }, 200);
+  }
+  openDialog() {
+    const dialogRef = this._dialog.open(DialogComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      this.lastDialogResult = result;
+    });
+  }
+
+  showSnackbar() {
+    this._snackbar.open('YUM SNACKS', 'CHEW');
+  }
+
+}
+@Component({
+
+  templateUrl: './dialog-overview-example-dialog.html'
+})
+export class DialogComponent {
+  constructor(public dialogRef: MdDialogRef<DialogComponent>) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import { AppComponent , DialogComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  entryComponents: [
+    DialogComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent ]
 })
 export class AppModule { }
